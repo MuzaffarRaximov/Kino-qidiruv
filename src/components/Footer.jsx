ee5d39eb13ba6249f071def2df77494ed8a3082b
MuzaffarRaximov <7ilm7kuch@gmail.com>
2022-12-02 15:21:22 +0300

    m2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='navbar bg-primary ps-2'>
+    <div className='navbar bg-primary ps-2 justify-content-center'>
       <p>Powered by Muzaffar</p>
     </div>
   )
